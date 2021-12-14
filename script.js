c2b9df59f14e6c210110065ea32e1c404cb0972b
henrique lauar <henriquelauar0@gmail.com>
2021-12-14 13:46:51 -0300

    adding code formatter

--- script.js
+++ script.js
@@ -10,6 +10,35 @@ function iniciaModal(modalID) {
     }
   }
 
-
 const btn = document.querySelector('.btn.btn-primary')
 btn.addEventListener('click', () => iniciaModal('modal-adicionar'));
+
+
+
+function addToTable() {
+
+    const nome = document.getElementById('nome').value;
+    const imagem = document.getElementById('imagem').value;
+    const descricao = document.getElementById('descricao').value;
+    const table = document.getElementById('myTable');
+
+    const tableSize = table.rows.length;
+    const row = table.insertRow(tableSize);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    row.id = tableSize;
+
+    
+    cell1.innerHTML = nome;
+    cell2.innerHTML = imagem;
+    cell3.innerHTML = descricao;
+
+
+    document.getElementById('nome').value = "";
+    document.getElementById('imagem').value = "";
+    document.getElementById('descricao').value = "";
+ 
+
+    return false;
+} 
