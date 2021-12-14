bebf8af0a891a7441cb9d9bb7e98a0e044fbf2de
henrique lauar <henriquelauar0@gmail.com>
2021-12-14 15:28:55 -0300

    fixed fakepath error in the input file

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function iniciaModal(modalID) {
-    const modal = document.getElementById(modalID);
-    if(modal) {
-    modal.classList.add('mostrar');
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == modalID || e.target.className == 'fechar') {
-            modal.classList.remove('mostrar')
-        }
-      });
-    }
+  const modal = document.getElementById(modalID);
+  if(modal) {
+  modal.classList.add('mostrar');
+  modal.addEventListener('click', (e) => {
+      if(e.target.id == modalID || e.target.className == 'fechar') {
+          modal.classList.remove('mostrar')
+      }
+    });
   }
+}
 
 const btn = document.querySelector('.btn.btn-primary')
 btn.addEventListener('click', () => iniciaModal('modal-adicionar'));
@@ -17,28 +17,28 @@ btn.addEventListener('click', () => iniciaModal('modal-adicionar'));
 
 function addToTable() {
 
-    const nome = document.getElementById('nome').value;
-    const imagem = document.getElementById('imagem').value;
-    const descricao = document.getElementById('descricao').value;
-    const table = document.getElementById('myTable');
+  const nome = document.getElementById('nome').value;
+  const imagem = document.getElementById('imagem').files[0].name; 
+  const descricao = document.getElementById('descricao').value;
+  const table = document.getElementById('myTable');
 
-    const tableSize = table.rows.length;
-    const row = table.insertRow(tableSize);
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    row.id = tableSize;
+  const tableSize = table.rows.length;
+  const row = table.insertRow(tableSize);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  row.id = tableSize
 
-    
-    cell1.innerHTML = nome;
-    cell2.innerHTML = imagem;
-    cell3.innerHTML = descricao;
+  
+  cell1.innerHTML = nome;
+  cell2.innerHTML = imagem;
+  cell3.innerHTML = descricao;
 
 
-    document.getElementById('nome').value = "";
-    document.getElementById('imagem').value = "";
-    document.getElementById('descricao').value = "";
- 
+  document.getElementById('nome').value = "";
+  document.getElementById('imagem').files[0].name="";
+  document.getElementById('descricao').value = "";
 
-    return false;
-} 
+
+  return false;
+}
