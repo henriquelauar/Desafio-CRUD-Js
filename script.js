47678d3460da4e5a3be8d57af9541d76b905efd1
henrique lauar <henriquelauar0@gmail.com>
2021-12-16 18:21:01 -0300

    adding localStorage and deleteService funtion

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const closeModal = () => {
     clearFields()
     document.getElementById('modal-adicionar').classList.remove('mostrar')
 }
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_service')) ?? []
+const setLocalStorage = (dbService) => localStorage.setItem("db_service", JSON.stringify(dbService))
+
+const deleteService = (index) => {
+  const dbService = readService ()
+  dbService.splice(index, 1)
+  setLocalStorage(dbService)
+} 
